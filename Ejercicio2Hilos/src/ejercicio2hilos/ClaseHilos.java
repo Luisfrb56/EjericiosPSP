7e74efe526be9d0be80e9b662d30cd295803b978
Luis Fernando <Luis Fernando@LAPTOP-D6M8IKR6>
2018-11-29 11:02:50 +0100

    Ejercicio 2 comentado

--- Ejercicio2Hilos/src/ejercicio2hilos/ClaseHilos.java
+++ Ejercicio2Hilos/src/ejercicio2hilos/ClaseHilos.java
@@ -4,17 +4,27 @@ package ejercicio2hilos;
 
 public class ClaseHilos extends Thread{
     String str;
-
+    /*
+    El hilo recoge un string al inicializarlo para ponerle nombre al hilo
+    */
     public ClaseHilos( String str) {
         this.str = str;
     }
+    /*
+    Entra en el run calcula un Sleep random para que se ejecutn los otros hilos interrumpidamente
+    */
+    
     public void run(){
         try{
             double k=Math.random();
          Thread.sleep((long) k);
-        
+        /*
+         for que hace que se ejecuten 5 veces todos los hilos.
+         */
         for(int i=0; i<5;i++){
-            
+            /*
+            Sout que muestra el numero por el que vamos de procesos y el nombre.
+            */
             System.out.println(i+" "+str);
             
         }
--- Ejercicio2Hilos/src/ejercicio2hilos/Ejercicio2Hilos.java
+++ Ejercicio2Hilos/src/ejercicio2hilos/Ejercicio2Hilos.java
@@ -5,6 +5,9 @@ public class Ejercicio2Hilos {
 
     
     public static void main(String[] args) {
+        /*
+        main que crea 4 hilos iguales
+        */
         new ClaseHilos("Primer Hilo").start();
         new ClaseHilos("Segundo Hilo").start();
         new ClaseHilos("Tercero Hilo").start();
