d0a6f2085f4e345f0caefbec40f6e5ea8f23b7be
Luis Fernando <Luis Fernando@LAPTOP-D6M8IKR6>
2018-11-29 11:36:11 +0100

    Ejercicio  5 comentado

--- Ejercicio5Hilos/src/ejercicio5hilos/Ejercicio5Hilos.java
+++ Ejercicio5Hilos/src/ejercicio5hilos/Ejercicio5Hilos.java
@@ -4,12 +4,19 @@ package ejercicio5hilos;
 public class Ejercicio5Hilos {
 
     public static void main(String[] args) throws InterruptedException {
+        /*
+        Inicializamos los dos hilos con nombre.
+        */
         Hilo h1 = new Hilo("fio1");
         Hilo h2 = new Hilo("fio2");
-        
+        /*
+        Le metemos nuestra prioridad a cada hilo
+        */
         h1.setPriority(1);
         h2.setPriority(10);
-        
+        /*
+        Iniciamos los hilos
+        */
         h2.start();
         h1.start();        
         
--- Ejercicio5Hilos/src/ejercicio5hilos/Hilo.java
+++ Ejercicio5Hilos/src/ejercicio5hilos/Hilo.java
@@ -4,7 +4,9 @@ package ejercicio5hilos;
 public class Hilo extends Thread{
     
     String nombre;
-
+    /*
+    tenemos nuestro hilo al que le pasamos un nombre
+    */
     public Hilo(String Nombre) {
         nombre = Nombre;
 
@@ -13,7 +15,9 @@ public class Hilo extends Thread{
     @Override
     public void run() {
         
-        
+        /*
+        Cuando se ejecuta muestra el nombre del hilo y un saludo.
+        */
         System.out.println("Ola, son o " + nombre);
 
     }
