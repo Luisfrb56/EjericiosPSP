acdbfce993cb14eb830139c5cd0c58b1667660b8
Luis Fernando <Luis Fernando@LAPTOP-D6M8IKR6>
2018-11-29 10:58:19 +0100

    Ejercicio 1 comentados

--- Ejericio1Hilos/src/ejericio1hilos/ClaseHilos.java
+++ Ejericio1Hilos/src/ejericio1hilos/ClaseHilos.java
@@ -1,17 +1,28 @@
 package ejericio1hilos;
 
-
+/*
+Clase hilo 
+*/
 public class ClaseHilos extends Thread{
     String str;
 
     public ClaseHilos( String str) {
         this.str = str;
     }
+    /*
+    Recoge un String que le damos al inicializarlo
+    Entra en el run al hacer el start y hace los dos hilos 10 veces 
+    */
     public void run(){
         try{
+            /*
+            Random para producir el sleep y que se ejecute el otro hilo
+            */
             double k=Math.random();
          Thread.sleep((long) k);
-        
+        /*
+         for para que se haga 10 veces el hilo
+         */
         for(int i=0; i<10;i++){
             
             System.out.println(i+" "+str);
--- Ejericio1Hilos/src/ejericio1hilos/Ejericio1Hilos.java
+++ Ejericio1Hilos/src/ejericio1hilos/Ejericio1Hilos.java
@@ -5,7 +5,9 @@ public class Ejericio1Hilos {
 
 
     public static void main(String[] args) {
-
+        /*
+        Inicializamos dos hilos iguales que se van a ejecutar interrumpidamente
+        */
         new ClaseHilos("Primer Hilo").start();
         new ClaseHilos("Segundo Hilo").start();
     }
